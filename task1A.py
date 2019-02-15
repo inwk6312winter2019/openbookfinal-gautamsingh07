d125fc0ca00f2b6b606f83526992bc7187b271d1
parthpatel041 <Parthkumar.Patel@dal.ca>
2019-02-15 10:48:07 -0800

    vowel done

---
+++ task1A.py
@@ -0,0 +1,28 @@
+fin1 = ("book1.txt", 'r')
+l1 = fin1.readline()
+string = [0]
+stringsplit = string.split
+
+fin2 = ("book2.txt" ,'r' )
+l2 = fin2.readline()
+
+fin3 = ("book3.txt" , 'r')
+l3 = fin3.readline()
+
+
+def unique_words():
+
+def count_the_article():
+ list=[]
+ for items in list:
+  print (len(item.split))
+
+def sorted_words():
+
+def character_word_count():
+ mystring=[]
+ dict=[]
+ for char in mystring:
+  dict[char] = char.count(char)
+
+def start_with_vov():
